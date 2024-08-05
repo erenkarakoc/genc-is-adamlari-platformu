619d89659a0f12d2b7bbdbb6ddeb326ddf3b14db
erenkarakoc <erenkarakoc@aol.com>
2024-08-06 01:06:58 +0300

    deploy.sh file adjusted

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Navigate to repository directory
-cd /var/www/genc-is-adamlari-platformu || { echo "Directory repository directory not found"; exit 1; }
+cd /var/www/genc-is-adamlari-platformu/ || { echo "Directory repository directory not found"; exit 1; }
 
 # Pull latest changes from the repository
 git pull origin main || { echo "Failed to pull changes"; exit 1; }
+
+# Navigate to app directory
+cd /var/www/genc-is-adamlari-platformu/arayuz || { echo "Directory repository directory not found"; exit 1; }
 
 # Install dependencies
 npm install || { echo "Failed to install dependencies"; exit 1; }
@@ -13,7 +16,7 @@ npm install || { echo "Failed to install dependencies"; exit 1; }
 npm run build || { echo "Failed to build app"; exit 1; }
 
 # Navigate to backend directory
-cd /var/www/genc-is-adamlari-platformu || { echo "Server directory not found"; exit 1; }
+cd /var/www/genc-is-adamlari-platformu/arayuz/backend || { echo "Server directory not found"; exit 1; }
 
 # Install server dependencies
 npm install || { echo "Failed to install server dependencies"; exit 1; }
